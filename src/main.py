c3c870a474979b33e78c84a791ae8d8f3ac30406
Neo-Teyrall <mailliw.marg@gmail.com>
2020-10-30 16:24:17 +0100

    finie pour ma partie tout est fonctionnel

--- src/main.py
+++ src/main.py
@@ -1,30 +1,8 @@
 import copy
 import datetime
+import sys
+from pytictoc import TicToc
 
-def creat_mat(seq):
-    out  = []
-    out.append(copy.deepcopy(seq))
-    for i in range(len(seq)-1):
-        seq = [seq[-1]] + seq[:-1]
-        out.append(copy.deepcopy(seq))
-    return out
-
-
-def get_trasforme(seq):
-    print("start transform")
-
-    seq = "$" +  seq
-    seq = list(seq)
-    mat = creat_mat(seq)
-    mat.sort()
-    T = []
-    c = 0
-    cp = len(mat)
-    for i in mat:
-        T.append(i[-1])
-        print(c,"/",cp,end = "\r")
-        c += 1 
-    return T
 
 def get_chain(T) -> list:
     index = []
@@ -33,26 +11,20 @@ def get_chain(T) -> list:
            "C" : 0,
            "G" : 0,
            "$" : 0}
-
     first_pos = {"A":-1, "T" : -1, "C": -1, "G": -1 ,"$" : -1}
-    
     sT = copy.deepcopy(T)
     sT.sort()
-
     for i in range(len(T)):
-        for k in first_pos.keys():
+       for k in first_pos.keys():
             if sT[i] == k:
                 if  first_pos[k] == -1:
                     first_pos[k] = i
             if T[i] == k : 
                 index.append(c_i[k])
                 c_i[k] += 1
-                
     f_mat = [sT, T , index, index]
-   
     pos = 0
     out = ""
-
     poss = []
     for i in range(len(T)):
         out = f_mat[1][pos] + out
@@ -63,17 +35,6 @@ def get_chain(T) -> list:
     out = out[1:]
     return(out,f_mat[3])
 
-def get_pos(seq):
-    T = get_trasforme(seq)
-    sort_T = copy.deepcopy(T)
-    sort_T.sort()
-    index = []
-    mat_counter_i, index = get_mat_counter(T)
-    first_pos = get_first_pos(T)
-
-
-    return(get_position(T,first_pos,index))
-
 
 def creat_counter(T,val = 0) :
     out = {}
@@ -81,17 +42,241 @@ def creat_counter(T,val = 0) :
         if i not in out :
             out.setdefault(i,val)
     return out
-        
-def get_first_pos(T):
-    sT = copy.deepcopy(T)
-    sT.sort()
-    out = creat_counter(T,-1)
+
+def find_read(read,mat_counter_i,first_pos,pos):
+    b , e = get_intervale_read(read, mat_counter_i, first_pos)
+    if b > e :
+        return None
+    elif b == e :
+        return(pos[b]+1)
+    else :
+        return([pos[b]+1,pos[e]+1])
+
+
+
+def get_position(T,first_pos,index):
+    position = [0]*len(T)
+    pos = 0
     for i in range(len(T)):
-        for k in out.keys():
-            if sT[i] == k:
-                if  out[k] == -1:
-                    out[k] = i
-    return out
+        position[pos] = len(T)-i-2
+        pos2 = first_pos[T[pos]] + index[pos] 
+        pos = pos2
+    return position
+
+
+
+################################################################################
+####################                get read                ####################
+################################################################################
+
+
+def get_intervale_read(read,mat_counter_i,first_pos):
+    read = read[::-1]
+    b : int = first_pos[read[0]]
+    e : int = first_pos[read[0]] + mat_counter_i[-1][read[0]] -1
+    for i in read[1:]:
+        b = first_pos[i] + mat_counter_i[b-1][i]
+        e = first_pos[i] + mat_counter_i[e][i] - 1
+    return(b,e)
+
+
+def get_intervale_read_sub(in_read, mat_counter_i,
+                           first_pos, subs = 1):
+    
+    # initialisation des paraetre
+    read = in_read[::-1] 
+    AN = ["A","T","C","G"]
+    pe = -1
+    pb = -1
+    sub_pos = -1
+    subs_an = []
+    i = 1
+    nb_sub = 0
+    ## debut
+    b : int = first_pos[read[0]]
+    e : int = first_pos[read[0]] + mat_counter_i[-1][read[0]] -1
+    
+    while i < len(read):
+        an = read[i] # l'acide aminé actuelle
+        pb , pe = b, e # sauvegardé les anciennes position.
+        b = first_pos[an] + mat_counter_i[b-1][an]
+        e = first_pos[an] + mat_counter_i[e][an] - 1
+        # si le read continure 
+        if i< len(read) and b <= e:
+            i += 1
+            print(i-1 ,"->", i )
+        # erreure dans le read
+        if b > e:
+            print(read,"to sub",read[i],i)
+            if sub_pos != i : # la position n'a pas encore été substituer 
+                sub_pos = i
+                subs_an = []
+                nb_sub += 1
+                # si le nombre de substitution est trop grand
+                if nb_sub > subs:
+                    print("trop de substitution")
+                    return(b,e)
+            # substituer la position #
+            #// ajouter l'acide nucleique dans la liste des acide nucléique déja
+            # tester \\#
+            subs_an.append(read[i])
+            # choisir un acide aminé dans les acide nucélique  pas encore tester
+            new_an = list(set(AN)-set(subs_an))[0]
+            # remplacer l'acide nucléique
+            read = read[:i] + new_an + read[i+1:]
+            ## retour au valeur de b et e de la position antétieru 
+            e = pe
+            b = pb
+            print(read,"subted",read[i])
+    return(b,e)
+
+def get_intervale_read_del(in_read, mat_counter_i, first_pos,
+                           dels = 1):
+    # initialisation des variable.
+    in_read_reverse = in_read[::-1]
+    read =in_read_reverse
+    pe = -1
+    pb = -1
+    sub_pos = -1
+    subs_an = []
+    i = 1
+    nb_dels = 0
+    b : int = first_pos[read[0]]
+    e : int = first_pos[read[0]] + mat_counter_i[-1][read[0]] -1
+    
+    while i < len(read):
+        an = read[i]
+        pb , pe = b, e
+        b = first_pos[an] + mat_counter_i[b-1][an]
+        e = first_pos[an] + mat_counter_i[e][an] - 1
+        # si la il ny a pas d'erreure dans l'alignement 
+        if i< len(read) and b <= e:
+            i += 1
+            print(i-1 ,"->", i )
+        # si l'aligenement est incorecte.
+        if b > e:
+            print(read,"to del",read[i],i)
+            nb_dels += 1 # augementé le compteur de déléttion 
+            
+            if nb_dels > dels: # Si trops 
+                print("trop de substitution")
+                return(b,e)
+            # suprimé la postion qui ne s'alligne pas
+            read = read[:i] + read[i+1:]
+            # reprendre la b et e de la position anterieurs.
+            e = pe
+            b = pb
+            print(read,"delated")
+    return(b,e)
+
+
+
+def get_intervale_read_ins(in_read, mat_counter_i, first_pos,
+                           inss = 1):
+    print()
+    in_read_reverse = in_read[::-1]
+    read =in_read_reverse
+    AN = ["A","T","C","G"]
+    pe = -1
+    pb = -1
+    ins_pos = -1
+    ins_an = []
+    b : int = first_pos[read[0]]
+    e : int = first_pos[read[0]] + mat_counter_i[-1][read[0]] -1
+    i = 1
+    nb_ins = 0
+    while i < len(read):
+        an = read[i]
+        pb , pe = b, e
+        b = first_pos[an] + mat_counter_i[b-1][an]
+        e = first_pos[an] + mat_counter_i[e][an] - 1
+        if i< len(read) and b <= e:
+            i += 1
+            print(i-1 ,"->", i )
+        if b > e:
+            if ins_pos != i :
+                print(read,"to to ins", i-1, i,)
+                ins_pos = i
+                ins_an = []
+                nb_ins += 1
+
+                if nb_ins > inss:
+                    print("trop d'insertion")
+                    return(b,e)
+                new_an = list(set(AN)-set(ins_an))[0]
+                ins_an.append(new_an)
+                read = read[:i] + new_an + read[i:]
+                e = pe
+                b = pb
+            else :
+                print(read,"to sub", read[i], i)
+                new_an = list(set(AN)-set(ins_an))[0]
+                ins_an.append(new_an)
+                read = read[:i] + new_an + read[i+1:]
+                e = pe
+                b = pb
+            print(read,"insered",read[i])
+    return(b,e)
+
+###############################################################################
+################################## Code projet ################################
+###############################################################################
+
+def get_seq(file_path : str):
+    seq = ""
+    with open(file_path) as filin:
+        lines = filin.readlines()
+        for i, line in enumerate(lines):
+            if line[0] == ">":
+                continue
+            seq += line[:-1]
+    return seq
+
+
+def get_T(seq:str) -> list:
+    print()
+    index = [] # creation d'une liste
+    seq +=  "$"
+    # remplissage de la liste avec une liste [posiiton , character]
+    for i, char in enumerate(seq):
+        if i == 0 : # pour la premiere position prendre le dernier charracter
+            c = seq[-1]
+        else :
+            c = seq[i-1] # pour les autre le caratere a la position antérieur
+        add = [i,c]
+        index.append(add) # ajouté a la liste
+    index  = index[1:] + [index[0]]
+    for i, char_i in enumerate(seq):
+        print("{:.2f} %".format(i/len(seq)*100),end = "\r") # affichage de progression
+        for j, char_j in enumerate(seq[i+1:]):
+            i2 = i
+            jb = i+j+1
+            j2 = jb
+            while j2+1 < len(seq)  and seq[i2] == seq[j2] :
+                i2 += 1
+                j2 += 1
+            if seq[i2] < seq[j2] and index[i][0] > index[jb][0]:
+                index[i][0], index[jb][0] = index[jb][0], index[i][0]
+            elif seq[i2] > seq[j2] and index[i][0] < index[jb][0]:
+                index[jb][0], index[i][0] = index[i][0], index[jb][0]
+            elif seq[i2] == seq[j2] and index[i][0] < index[jb][0] and j2 == len(seq)-1:
+                index[j2][0], index[i2][0] = index[i2][0], index[j2][0]
+
+    for i in range(len(index)):
+        if i  == 0 :
+            last = index[-1][0]
+            index[-1][0] = index[i][0]
+        elif i== len(index)-1:
+            index[i-1][0] = last
+        else:
+            index[i-1][0] = index[i][0]
+    index.sort()
+    for i,val in enumerate(index) :
+        index[i] = val[1]
+    return index
+
+
+
 def get_mat_counter(T):
     out = []
     index = []
@@ -104,78 +289,21 @@ def get_mat_counter(T):
         out.append(copy.deepcopy(counter_i))
     return(out,index)
 
-def find_read(T,sort_T,read,mat_counter_i,index,first_pos,pos):
-    read = read[::-1]
-    b , e = get_intervale_read(read, mat_counter_i, first_pos)
-    if b > e :
-        return None
-    elif b == e :
-        return(pos[b]+1)
-    else :
-        return([pos[b]+1,pos[e]+1])
-
-def get_position(T,first_pos,index):
-    position = [0]*len(T)
-    pos = 0
+def get_first_pos(T):
+    sT = copy.deepcopy(T)
+    sT.sort()
+    out = creat_counter(T,-1)
     for i in range(len(T)):
-        position[pos] = len(T)-i-2
-        pos2 = first_pos[T[pos]] + index[pos] 
-        pos = pos2
-    return position
+        for k in out.keys():
+            if sT[i] == k:
+                if  out[k] == -1:
+                    out[k] = i
+    return out
 
-def get_intervale_read(read,mat_counter_i,first_pos):
-    b : int = first_pos[read[0]]
-    e : int = first_pos[read[0]] + mat_counter_i[-1][read[0]] -1
-    for i in read[1:]:
-        b = first_pos[i] + mat_counter_i[b-1][i]
-        e = first_pos[i] + mat_counter_i[e][i] - 1
-    return(b,e)
-    pass
-
-import sys
-from pytictoc import TicToc
-import char
-from char import i
 
 ###############################################################################
-################################## Code projet ################################
+##########                          Main                            ###########
 ###############################################################################
-
-
-def get_T(seq) -> list:
-    index = [] # creation d'une liste
-    # remplissage de la liste avec une liste [posiiton , character]
-    for i, char in enumerate(seq):
-        if i == 0 : # pour la premiere position prendre le dernier charracter
-            c = seq[-1]
-        else :
-            c = seq[i-1] # pour les autre le caratere a la position antérieur
-        add = [i,c]
-        index.append(add) # ajouté a la liste
-        
-    for i, char_i in enumerate(seq):
-        print("{:.2f} %".format(i/len(seq)*100),end = "\r") # affichage de progression
-        for j, char_j in enumerate(seq[i+1:]):
-            i2 = i
-            j2 = j+i+1
-            while j2+1 < len(seq)  and seq[i2] != seq[j2]  and i2 < j:
-                i2 += 1
-                j2 += 1
-            if seq[i2] < seq[j2] and index[i2][0] > index[j2][0]:
-                index[i2][0], index[j2][0] = index[j2][0], index[i2][0]
-            elif seq[i2] > seq[j2] and index[i2][0] < index[j2][0]:
-                index[j2][0], index[i2][0] = index[i2][0], index[j2][0]
-            elif seq[i2] == seq[j2] and index[i2][0] < index[j2][0] and j2 == len(seq)-1:
-                index[j2][0], index[i2][0] = index[i2][0], index[j2][0]
-
-
-    index.sort()
-    # récupéré la transformer.
-    for i,val in enumerate(index) :
-        index[i] = val[1]
-    return index
-
-
 
 if __name__ == "__main__" :
     in_seq= {}
@@ -187,7 +315,6 @@ if __name__ == "__main__" :
         pos = list(lines[1][:-2].split(" "))
         for i, p  in enumerate(pos):
             pos[i] = int(p)
-
     ok = False
     key = ""
     in_read = {}
@@ -205,49 +332,17 @@ if __name__ == "__main__" :
                 string += line[:-1]
         in_read.setdefault(key,string)
 
-    out1 = {}
-    out2 = {}
-    c = 0
-    ct = len(in_read)
-    get_T()
-    from time import time
-    t1 = time()
-    # do something
-    t2 = time()
-    sort_T = copy.deepcopy(T)
-    sort_T.sort()
-    mat_counter_i, index = get_mat_counter(T)
-    first_pos = get_first_pos(T)
-    pos = get_position(T,first_pos,index)
-    for j in in_read:
-        s = (t2 - t1) * (ct-c)
-        m = s/ 60
-        H = int(m/60)
-        #print("{:2}:{:2}:{:2}".format(H,int(m%60),int(s%60)), "\t seconde",end="\r")
-        print("{:.2f}".format(c/ct*100),"%",end = "\r")
-        t1 = time()
-        c +=1
-        t.tic()
-        a = find_read(T,sort_T, in_read[j],mat_counter_i,index,first_pos,pos)
-        if a== None:
-            t2 = time()
-            continue
-        elif isinstance(a,list):
-            out2.setdefault(j,a)
-        else:
-            out1.setdefault(j,a)
-        t2 = time()
-    print(out1)
-    print(out2)
-    seq = ""
-    with open("./Hu-1.fasta") as filin:
-        lines = filin.readlines()
-        for i, line in enumerate(lines):
-            if line[0] == ">":
-                continue
-            seq += line[:-1] 
-    a = get_T(seq)
+    seq = get_seq("./Hu-1.fasta")
+    seq = "ATATCGT"
+    T = get_T(seq)
 
-            
-            
-    
+    mat_counter_i,index = get_mat_counter(T)
+    first_pos  = get_first_pos(T)
+    pos = get_position(T,first_pos,index)
+    interval = find_read("ATC", mat_counter_i,
+                         first_pos, pos)
+
+    print(get_intervale_read_sub("TACG",mat_counter_i,first_pos))
+    print(get_intervale_read_del("ACCG",mat_counter_i,first_pos,dels = 2))
+    print(get_intervale_read_ins("AG",mat_counter_i,first_pos,inss = 1))
+
